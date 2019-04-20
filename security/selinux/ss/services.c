338b3e08135b89fd2a067829ab49893bda38d425
Tyler Nijmeh <tylernij@gmail.com>
2019-04-20 15:45:58 -0700

    selinux: Allow audit to be disabled
    SELinux doesn't actually need audit support. In fact, auditing is not
    particularly useful on production kernels, where minimal debugging is
    needed.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3576,6 +3576,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3598,6 +3599,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
