c1aa89a3c5f417b208f636c8460e7a568913c8b3
ris <rissu.ntk@gmail.com>
2026-05-24 13:55:39 +0700

    block: bfq: fix bfq_log_bfqq function when CONFIG_BFQ_GROUP_IOSCHED disabled
    ../block/bfq-iosched.c:898:1: error: expected 'while' in do/while loop
      898 | static struct request *bfq_find_next_rq(struct bfq_data *bfqd,
          | ^
    ../block/bfq-iosched.c:894:2: note: to match this 'do'
      894 |         bfq_log_bfqq(bfqq->bfqd, bfqq, "check_fifo: returned %p", rq);
          |         ^
    ../block/bfq-iosched.h:1080:48: note: expanded from macro 'bfq_log_bfqq'
     1080 | #define bfq_log_bfqq(bfqd, bfqq, fmt, args...) do {     \
          |                                                ^
    ../block/bfq-iosched.c:6901:61: error: expected '}'
     6901 | MODULE_DESCRIPTION("MQ Budget Fair Queueing I/O Scheduler");
          |                                                             ^
    ../block/bfq-iosched.c:881:1: note: to match this '{'
      881 | {
          | ^
    2 errors generated.
    make[2]: *** [../scripts/Makefile.build:338: block/bfq-iosched.o] Error 1
    make[1]: *** [/home/runner/work/asus_kernel_action/asus_kernel_action/kernel_root/Makefile:1336: block] Error 2
    make[1]: *** Waiting for unfinished jobs....
    
    Change-Id: I3d401212e1b2080914005e26ff84253da79d2e66
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- block/bfq-iosched.h
+++ block/bfq-iosched.h
@@ -1082,7 +1082,9 @@ struct bfq_group *bfqq_group(struct bfq_queue *bfqq);
 	bfq_pid_to_str((bfqq)->pid, pid_str, MAX_PID_STR_LENGTH);	\
 	blk_add_trace_msg((bfqd)->queue, "bfq%s%c " fmt, pid_str,	\
 			bfq_bfqq_sync((bfqq)) ? 'S' : 'A',		\
-				##args)
+				##args)					\
+} while (0)
+
 #define bfq_log_bfqg(bfqd, bfqg, fmt, args...)		do {} while (0)
 
 #endif /* CONFIG_BFQ_GROUP_IOSCHED */
